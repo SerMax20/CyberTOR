bd3b52ec206dce2dc11ce023ca0d7303a667c41b
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-07-18 12:05:59 +0000

    Update footer.js

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -5,19 +5,7 @@ function footer() {
     <div className="nav">
       <div className="content">
         <div className="nav-logo">
-          <h1 className="h4 m-0">Copyright @ 2020. <a className="alpha" href="https://github.com/shakilofficial0/Heroku-CyberTOR">CyberTOR-Beta</a></h1>
-        </div>
-        <div className="nav-links" style={{flexDirection: "row"}}>
-          <a className="btn primary" href="https://www.facebook.com/shakilofficialdll" style={{color: "#f7fafc"}}>
-    <span className="bnt-icon">
-        <ion-icon name="logo-facebook" />
-      </span> Facebook
-    </a>
-     <a className="btn" href="https://github.com/shakilofficial0" style={{color: "#f7fafc"}}>
-    <span className="bnt-icon">
-        <ion-icon name="logo-github" />
-      </span> Github
-    </a>
+          <h1 className="h4 m-0">Copyright @ 2020. <a className="alpha" href="">Elakiri-Beta</a></h1>
         </div>
       </div>
     </div>
